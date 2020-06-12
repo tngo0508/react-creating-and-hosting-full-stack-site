e0308ff0fbbe241c217f046e4caa48111e8f8101
Thomas Ngo <tngo0508@gmai.com>
2020-06-11 23:36:11 -0700

    creating and linking the articles list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesList from "./pages/ArticlesList";
+import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <NavBar />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article/:name" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPage = () => (
+  <>
+    <h1>About Me</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit at qui quis
+      itaque, nostrum blanditiis! Illo adipisci nesciunt vero minima cum, dolore
+      temporibus maiores quia fuga placeat, a minus cumque.
+    </p>
+  </>
+);
+
+export default AboutPage;
