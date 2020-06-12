352a84d2992bc8a33102368ed215349e0cf01ff4
Thomas Ngo <tngo0508@gmai.com>
2020-06-11 22:03:29 -0700

    creating the app component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit at qui quis
+      itaque, nostrum blanditiis! Illo adipisci nesciunt vero minima cum, dolore
+      temporibus maiores quia fuga placeat, a minus cumque.
+    </p>
+  </>
+);
+
+export default HomePage;
