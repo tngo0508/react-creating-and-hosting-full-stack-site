98536660fb090bb606806cb11f3b74edf24d6320
Thomas Ngo <tngo0508@gmai.com>
2020-06-11 23:45:00 -0700

    Making the articles list modular

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ArticlesList from "../components/ArticlesList";
 import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
@@ -6,12 +7,19 @@ const ArticlePage = ({ match }) => {
   const article = articleContent.find((article) => article.name === name);
 
   if (!article) return <h1>Article does not exist</h1>;
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
   return (
     <>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other Articles: </h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 };
